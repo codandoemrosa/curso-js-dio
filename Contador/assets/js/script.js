cce091494bae54fd331bb241e4ffdf72fa859e2c
codandoemrosa <isadorascerdeira@gmail.com>
2022-07-02 23:53:25 -0400

    change the script for the professor's model

--- Contador/assets/js/script.js
+++ Contador/assets/js/script.js
@@ -1,12 +1,21 @@
-var currentNumberWrapper = document.getElementById('currentNumber');
-var currentNumber = 0;
+let count = 0;
+
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
 
 function increment(){
-  currentNumber = currentNumber + 1;
-  currentNumberWrapper.innerHTML = currentNumber;
+  count++;
+  CURRENT_NUMBER.innerHTML = count;
 }
 
 function decrement(){
-  currentNumber = currentNumber -1;
-  currentNumberWrapper.innerHTML = currentNumber;
+ count--;
+ CURRENT_NUMBER.innerHTML = count;
 }
+
+
+
+
+/* tente implementar addEventListener
+condicionais para desabilitar incremento ou decremento quando COUNT for um valor diferente
+mudar a cor do texto em CURRENT_NUMBER para vermelho quando o número for negativo. */
